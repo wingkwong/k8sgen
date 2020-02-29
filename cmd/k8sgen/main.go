a76d02ad5db8283d631114e1b9a9069f01bd89af
wingkwong <wingkwong.code@gmail.com>
2020-02-29 22:19:37 +0800

    feat: add cli root

---
+++ cmd/k8sgen/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/wingkwong/k8sgen/internal/pkg/cli"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	cmd := buildRootCmd()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func buildRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:              "k8sgen",
+		Short:            "Generating Kubenetes Resource Configurations in an Interactive CLI",
+		Example:          `k8sgen`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {},
+		SilenceUsage:     true,
+	}
+
+	cmd.AddCommand(cli.BuildCreateCmd())
+	cmd.AddCommand(cli.BuildJumpStartCmd())
+
+	return cmd
+}
