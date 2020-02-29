0e8884637897d6d903957357d837baba002fab7c
wingkwong <wingkwong.code@gmail.com>
2020-02-29 22:20:19 +0800

    feat: add resources

---
+++ internal/pkg/cli/resource.go
@@ -0,0 +1,80 @@
+package cli
+
+const (
+	clusterRoleName         = "ClusterRole"
+	clusterRoleBindingName  = "ClusterRoleBinding"
+	configmapName           = "Configmap"
+	deploymentName          = "Deployment"
+	jobName                 = "Job"
+	namespaceName           = "Namespace"
+	podDisruptionBudgetName = "PodDisruptionBudget"
+	priorityClassName       = "PriorityClass"
+	quotaName               = "Quota"
+	roleName                = "Role"
+	roleBindingName         = "RoleBinding"
+	secretName              = "Secret"
+	serviceName             = "Service"
+	serviceAccountName      = "ServiceAccount"
+)
+
+const (
+	clusterRoleCmdName         = "clusterrole"
+	clusterRoleBindingCmdName  = "clusterrolebinding"
+	configmapCmdName           = "configmap"
+	deploymentCmdName          = "deployment"
+	jobCmdName                 = "job"
+	cmdNamespaceCmdName        = "namespace"
+	podDisruptionBudgetCmdName = "poddisruptionbudget"
+	priorityClassCmdName       = "priorityclass"
+	quotaCmdName               = "quota"
+	roleCmdName                = "role"
+	roleBindingCmdName         = "rolebinding"
+	secretCmdName              = "secret"
+	serviceCmdName             = "service"
+	serviceAccountCmdName      = "serviceaccount"
+)
+
+const (
+	jsonName           = "json"
+	yamlName           = "yaml"
+	nameName           = "name"
+	goTemplateName     = "go-template"
+	goTemplateFileName = "go-template-file,"
+	templateName       = "template"
+	templateFileName   = "templatefile"
+	jsonPathName       = "jsonpath"
+	jsonPathFileName   = "jsonpath-file"
+)
+
+func getKindNames() []string {
+	return []string{
+		clusterRoleName,
+		clusterRoleBindingName,
+		configmapName,
+		deploymentName,
+		jobName,
+		namespaceName,
+		podDisruptionBudgetName,
+		priorityClassName,
+		quotaName,
+		roleName,
+		roleBindingName,
+		secretName,
+		serviceName,
+		serviceAccountName,
+	}
+}
+
+func getOutputFormats() []string {
+	return []string{
+		jsonName,
+		yamlName,
+		nameName,
+		goTemplateName,
+		goTemplateFileName,
+		templateName,
+		templateFileName,
+		jsonPathName,
+		jsonPathFileName,
+	}
+}
