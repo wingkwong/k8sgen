8a640335fdb129c7de7a07b072ac38803601b12a
wingkwong <wingkwong.code@gmail.com>
2020-03-01 11:16:58 +0800

    chore: format code

--- internal/pkg/cli/prompter.go
+++ internal/pkg/cli/prompter.go
@@ -1,6 +1,8 @@
 package cli
 
-import "github.com/wingkwong/k8sgen/third_party/term/prompt"
+import (
+	"github.com/k8sgen/third_party/term/prompt"
+)
 
 type prompter interface {
 	Get(message, help string, validator prompt.ValidatorFunc, opts ...prompt.GetOption) (string, error)
