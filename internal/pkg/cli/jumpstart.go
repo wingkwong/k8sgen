6322bbe85a387c8ca78744d956d0863b6ecf9479
wingkwong <wingkwong.code@gmail.com>
2020-03-01 16:15:23 +0800

    feat: add available resources

--- internal/pkg/cli/jumpstart.go
+++ internal/pkg/cli/jumpstart.go
@@ -58,8 +58,60 @@ func (o *jumpStartOpts) askKindName() error {
 
 func (o *jumpStartOpts) Execute() error {
 	switch o.KindName {
+		case clusterRoleName:
+			if err := o.ExecuteJumpStartClusterRoleCmd(); err != nil {
+				return err
+			}
+		case clusterRoleBindingName:
+			if err := o.ExecuteJumpStartClusterRoleBindingCmd(); err != nil {
+				return err
+			}
+		case configmapName:
+			if err := o.ExecuteJumpStartConfigMapCmd(); err != nil {
+				return err
+			}
 		case deploymentName:
 			if err := o.ExecuteJumpStartDeploymentCmd(); err != nil {
+				return err
+			}
+		case jobName:
+			if err := o.ExecuteJumpStartJobCmd(); err != nil {
+				return err
+			}
+		case namespaceName:
+			if err := o.ExecuteJumpStartNamespaceCmd(); err != nil {
+				return err
+			}
+		case podDisruptionBudgetName:
+			if err := o.ExecuteJumpStartPodDisruptionBudgetCmd(); err != nil {
+				return err
+			}
+		case priorityClassName:
+			if err := o.ExecuteJumpStartPriorityClassCmd(); err != nil {
+				return err
+			}
+		case quotaName:
+			if err := o.ExecuteJumpStartQuotaCmd(); err != nil {
+				return err
+			}
+		case roleName:
+			if err := o.ExecuteJumpStartRoleCmd(); err != nil {
+				return err
+			}
+		case roleBindingName:
+			if err := o.ExecuteJumpStartRoleBindingCmd(); err != nil {
+				return err
+			}
+		case secretName:
+			if err := o.ExecuteJumpStartSecretCmd(); err != nil {
+				return err
+			}
+		case serviceName:
+			if err := o.ExecuteJumpStartServiceCmd(); err != nil {
+				return err
+			}
+		case serviceAccountName:
+			if err := o.ExecuteJumpStartServiceAccountCmd(); err != nil {
 				return err
 			}
 		default:
