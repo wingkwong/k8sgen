9236ca2084697cb7351085c0f59f837e9ad1c14b
wingkwong <wingkwong.code@gmail.com>
2020-03-01 11:15:52 +0800

    chore: move jumpstart/jumpstart to jumpstart

--- internal/pkg/cli/jumpstart.go
+++ internal/pkg/cli/jumpstart.go
@@ -1,9 +1,73 @@
 package cli
 
 import (
-	"github.com/k8sgen/internal/pkg/cli/jumpstart"
+	"errors"
+	"fmt"
 	"github.com/spf13/cobra"
 )
+
+type DploymentCmdOpts struct {
+	deploymentName string
+	imageName string
+	outputFormat string
+	outputPath string
+}
+
+type jumpStartVars struct {
+	*GlobalOpts
+	*DploymentCmdOpts
+	KindName          string
+}
+
+type jumpStartOpts struct {
+	jumpStartVars
+}
+
+func newJumpStartOpts(vars jumpStartVars) (*jumpStartOpts, error) {
+	return &jumpStartOpts{
+		jumpStartVars: vars,
+	}, nil
+}
+
+func (o *jumpStartOpts) Ask() error {
+	if err := o.askKindName(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *jumpStartOpts) askKindName() error {
+	if o.KindName != "" {
+		return nil
+	}
+
+	names := getKindNames()
+
+	if len(names) == 0 {
+		return errors.New("No object is found")
+	}
+
+	selectedKindName, err := o.prompt.SelectOne("What kind of object you want to create?", "", names)
+	if err != nil {
+		return fmt.Errorf("Select kind name: %w", err)
+	}
+	o.KindName = selectedKindName
+	return nil
+}
+
+func (o *jumpStartOpts) Execute() error {
+	switch o.KindName {
+		case clusterRoleName:
+			if err := o.ExecuteClusterRoleCmd(); err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("No execution available for kind: %s", o.KindName)
+	}
+
+	return nil
+}
 
 func BuildJumpStartCmd() *cobra.Command {
 	vars := jumpStartVars{
