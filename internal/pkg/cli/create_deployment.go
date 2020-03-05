1c551697e6a3201b8222ce0bc4b8c88361110415
wingkwong <wingkwong.code@gmail.com>
2020-03-05 22:27:45 +0800

    refak: revise create deployment structure

--- internal/pkg/cli/create_deployment.go
+++ internal/pkg/cli/create_deployment.go
@@ -11,7 +11,9 @@ import (
 
 func int32Ptr(i int32) *int32 { return &i }
 
-func (o *askOpts) AskDeploymentSpecOpts() error {
+func (o *askOpts) ExecuteCreateDeploymentSpec() error {
+	var objectMeta metav1.ObjectMeta
+	var spec appsv1.DeploymentSpec
 
 	if err := o.Ask("RequireObjectMeta"); err != nil {
 		return err
@@ -19,7 +21,7 @@ func (o *askOpts) AskDeploymentSpecOpts() error {
 
 	if o.requireObjectMeta {
 		// Name
-		if err := o.Ask("Kind"); err != nil {
+		if err := o.Ask("ObjectMetaName"); err != nil {
 			return err
 		}
 
@@ -27,6 +29,12 @@ func (o *askOpts) AskDeploymentSpecOpts() error {
 		if err := o.Ask("Namespace"); err != nil {
 			return err
 		}
+
+		// Deployment > ObjectMeta
+		objectMeta = metav1.ObjectMeta{
+			Name: o.Name,
+		}
+
 	}
 
 	if err := o.Ask("RequireDeploymentSpec"); err != nil {
@@ -35,13 +43,75 @@ func (o *askOpts) AskDeploymentSpecOpts() error {
 
 	if o.requireDeploymentSpec {
 		// Replicas
+		if err := o.Ask("Replicas"); err != nil {
+			return err
+		}
 		// Selector
 		// Template
 		// Strategy
 		// MinReadySeconds
+		if err := o.Ask("MinReadySeconds"); err != nil {
+			return err
+		}
 		// RevisionHistoryLimit
+		if err := o.Ask("RevisionHistoryLimit"); err != nil {
+			return err
+		}
 		// Paused
+		if err := o.Ask("Paused"); err != nil {
+			return err
+		}
 		// ProgressDeadlineSeconds
+		if err := o.Ask("ProgressDeadlineSeconds"); err != nil {
+			return err
+		}
+
+		// Deployment > DeploymentSpec > PodTemplateSpec > PodSpec
+		podSpec := apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Name:  "web",
+					Image: "nginx:1.12",
+					Ports: []apiv1.ContainerPort{
+						{
+							Name:          "http",
+							Protocol:      apiv1.ProtocolTCP,
+							ContainerPort: 80,
+						},
+					},
+				},
+			},
+		}
+
+		// Deployment > DeploymentSpec > PodTemplateSpec
+		podTemplateSpec := apiv1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{
+					"app": "demo",
+				},
+			},
+			Spec: podSpec,
+		}
+
+		// Deployment > DeploymentSpec > LabelSelector
+		labelSelector := &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "demo",
+			},
+		}
+
+		// Deployment > DeploymentSpec
+		spec = appsv1.DeploymentSpec{
+			// FIXME: ambiguous selector
+			// Replicas: int32Ptr(o.Replicas),
+			Selector: labelSelector,
+			Template: podTemplateSpec,
+			// Strategy:                nil,
+			MinReadySeconds:         o.MinReadySeconds,
+			RevisionHistoryLimit:    o.RevisionHistoryLimit,
+			Paused:                  o.Paused,
+			ProgressDeadlineSeconds: o.ProgressDeadlineSeconds,
+		}
 
 	}
 
@@ -60,67 +130,8 @@ func (o *askOpts) AskDeploymentSpecOpts() error {
 		// CollisionCount
 	}
 
-	if err := o.Ask("DeploymentName"); err != nil {
-		return err
-	}
-
-	if err := o.Ask("Image"); err != nil {
-		return err
-	}
-
 	if err := o.AskOutputInfo(); err != nil {
 		return err
-	}
-
-	return nil
-}
-
-func (o *askOpts) ExecuteDeploymentSpec() error {
-
-	// Deployment > DeploymentSpec > PodTemplateSpec > PodSpec
-	podSpec := apiv1.PodSpec{
-		Containers: []apiv1.Container{
-			{
-				Name:  "web",
-				Image: "nginx:1.12",
-				Ports: []apiv1.ContainerPort{
-					{
-						Name:          "http",
-						Protocol:      apiv1.ProtocolTCP,
-						ContainerPort: 80,
-					},
-				},
-			},
-		},
-	}
-
-	// Deployment > DeploymentSpec > LabelSelector
-	labelSelector := &metav1.LabelSelector{
-		MatchLabels: map[string]string{
-			"app": "demo",
-		},
-	}
-
-	// Deployment > DeploymentSpec > PodTemplateSpec
-	podTemplateSpec := apiv1.PodTemplateSpec{
-		ObjectMeta: metav1.ObjectMeta{
-			Labels: map[string]string{
-				"app": "demo",
-			},
-		},
-		Spec: podSpec,
-	}
-
-	// Deployment > ObjectMeta
-	objectMeta := metav1.ObjectMeta{
-		Name: "demo-deployment",
-	}
-
-	// Deployment > DeploymentSpec
-	spec := appsv1.DeploymentSpec{
-		Replicas: int32Ptr(2),
-		Selector: labelSelector,
-		Template: podTemplateSpec,
 	}
 
 	// Deployment
@@ -136,17 +147,7 @@ func (o *askOpts) ExecuteDeploymentSpec() error {
 	}
 	fmt.Println(string(b))
 
-	return nil
-}
-
-func (o *askOpts) ExecuteCreateDeploymentSpec() error {
-	if err := o.AskDeploymentSpecOpts(); err != nil {
-		return err
-	}
-
-	if err := o.ExecuteDeploymentSpec(); err != nil {
-		return err
-	}
+	// TODO: yaml conversion
 
 	return nil
 }
